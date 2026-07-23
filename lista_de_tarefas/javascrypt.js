6d3790f19d82f407bb063f16ad12bb36bc67c474
GuilhermeDantass543 <antonio.guilhermevieira543@gmail.com>
2026-07-23 15:38:40 -0300

    finalizando lista de tarefas

--- lista_de_tarefas/javascrypt.js
+++ lista_de_tarefas/javascrypt.js
@@ -29,3 +29,68 @@ input.addEventListener('keydown', function(evento) {
         adicionarTarefa();   
     }
 });
+
+//função para deletar
+
+const Delete = document.querySelector('.delete');
+
+let modoDeletar = false; 
+
+Delete.addEventListener('click', function(){
+    modoDeletar = !modoDeletar; 
+    
+    Delete.classList.toggle('btn-ativo', modoDeletar);
+});
+
+lista.addEventListener('click', function(evento){
+    if (modoDeletar === true && evento.target.tagName === 'P'){
+        evento.target.remove(); 
+    
+        modoDeletar = false; 
+        
+        Delete.classList.remove('btn-ativo');
+    }
+});
+
+//função editar
+
+const Editar = document.querySelector('.editar');
+let modeditar = false;
+
+Editar.addEventListener('click', function(){
+    modeditar = !modeditar; 
+    Editar.classList.toggle('btn-ativo', modeditar);
+});
+
+lista.addEventListener('click', function(evento){
+    
+    if (modeditar === true && evento.target.tagName === 'P'){
+        
+        const tarefaclick = evento.target;
+
+        tarefaclick.contentEditable = true;
+        
+        tarefaclick.focus();
+
+        modeditar = false; 
+        Editar.classList.remove('btn-ativo');
+      
+        tarefaclick.addEventListener('blur', function() {
+            tarefaclick.contentEditable = false;
+        }, { once: true });
+
+        tarefaclick.addEventListener('keydown', function(e) {
+            if (e.key === 'Enter') {
+                e.preventDefault();
+
+                tarefaclick.blur();
+            }
+        });
+    }
+});
+
+
+
+
+
+
